1c59a45ef1e2e27a612d1e073fd07dc9ef89de5f
Maciek <gabmaciej@gmail.com>
2020-09-05 03:09:17 +0200

    Add user data update

--- src/main/java/com/webapp/rentalapp/controller/WelcomeController.java
+++ src/main/java/com/webapp/rentalapp/controller/WelcomeController.java
@@ -8,6 +8,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -38,6 +40,7 @@ public class WelcomeController {
 
 		logger.info(String.valueOf(request.isUserInRole("ROLE_ADMIN")));
 
+		logger.info(String.valueOf(request.getUserPrincipal()));
 		if(request.isUserInRole("ROLE_ADMIN"))
 		{
 			return "adminPage";
@@ -67,6 +70,12 @@ public class WelcomeController {
 		return "login";
 	}
 
+	@RequestMapping(value = "/updateUser", method = RequestMethod.POST)
+	public String updateUserData(@ModelAttribute("client") Client client){
+		clientRepository.save(client);
+		return "redirect:/";
+	}
+
 	//Users Controllers
 	@RequestMapping("/welcome/showDb")
 	public String showRentalDB(Model model	,SecurityContextHolderAwareRequestWrapper request) {
@@ -74,10 +83,23 @@ public class WelcomeController {
 		model.addAttribute("equipment", equipmentRepository.findAll());
 		return "rentalDB";
 	}
-
-
-
-
+	//Currently not used
+//	@RequestMapping(value = "/editUser", method = RequestMethod.GET)
+////	public  String currentData(MyUserDetailsService myUserDetailsService, Model model, SecurityContextHolderAwareRequestWrapper request){
+//	public  String currentData(Model model	,SecurityContextHolderAwareRequestWrapper request){
+//
+//		System.out.println(request.getUserPrincipal());
+//		model.addAttribute("clients",clientRepository.findByUsername(request.getRemoteUser()));
+//		logger.info(String.valueOf(model));
+////		myUserDetailsService.loadUserByUsername(request.getRemoteUser());
+//		return "editUser";
+//	}
+//
+//	@RequestMapping(value = "/editUser", method = RequestMethod.POST)
+//	public  String updatedData(){
+////		myUserDetailsService.loadUserByUsername(request.getRemoteUser());
+//		return "welcome";
+//	}
 
 	//Admin Controllers
 	@RequestMapping("/admin")
@@ -86,13 +108,29 @@ public class WelcomeController {
 	}
 
 	@RequestMapping("/admin/showUsers")
-	public String getCountries(Model model	,SecurityContextHolderAwareRequestWrapper request) {
-
-
+	public String getCountries(Model model	, SecurityContextHolderAwareRequestWrapper request) {
+//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//		System.out.println(request.getRemoteUser());
+//		System.out.println(auth.getDetails().toString());
+//		System.out.println(request.getUserPrincipal());
+//		System.out.println(request.getUserPrincipal().getName());
 
 		model.addAttribute("clients", clientRepository.findAll());
 		logger.info(String.valueOf(model));
 		return "showUsers";
 	}
+
+	@RequestMapping("/editUsers/{id}")
+	public String editUserPage(@PathVariable long id, Model model) {
+		Client client = clientRepository.getOne(id);
+		model.addAttribute("clients",client);
+		logger.info(String.valueOf(model));
+		return "editUser";
+	}
+
 }
 
+//TODO Wszystkie html są do ujednolicenia żeby łatwiej się robiło frontend!
+//FIXME Edycja Prawie działa:
+// - znika imię - pewnie przez to że jest jako hasło gdzieś tam w tej pamięci
+// - pierdoli się rola, jak jest przesyłana taka sama jak była to się zeruje a jak wstawiam 1,2,3 to wywala błędy
